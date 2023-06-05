d11a3f901ec3b6445da2430193242331125e95cc
victorqedu <victor@caido.ro>
2023-06-05 08:43:53 +0300

    Diverse

--- src/main/java/com/caido/iqtest/NewErrorHandler.java
+++ src/main/java/com/caido/iqtest/NewErrorHandler.java
@@ -1,5 +1,7 @@
 package com.caido.iqtest;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,10 @@ public class NewErrorHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handle(Exception ex, HttpServletRequest request, HttpServletResponse response) {
         System.out.println("Some error "+ex);
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        System.out.println("Some error "+sw.toString());
 //        if (ex instanceof NullPointerException) {
 //            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 //        }
